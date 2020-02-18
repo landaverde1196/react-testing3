061cf280d037b821f3c0fb419b82744a2f77daba
landaverde1196 <alejndro1196@gmail.com>
2020-02-17 23:17:34 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import UserInput from "./UserInput/UserInput";
+import UserOutput from "./UserOutput/UserOutput";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [{ userName: "Phil" }, { userName: "Mark" }, { userName: "Flex" }]
+  };
+
+  UserNameHandler = event => {
+    this.setState({
+      users: [{ userName: event.target.value }, { userName: "Mark" }, { userName: "Flex" }]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <UserInput
+          userName={this.state.users[0].userName}
+          changed={this.UserNameHandler.bind(this)}
+        />
+        <UserOutput userName={this.state.users[0].userName} />
+        <UserOutput userName={this.state.users[1].userName} />
+        <UserOutput userName={this.state.users[2].userName} />
+      </div>
+    );
+  }
 }
 
 export default App;
